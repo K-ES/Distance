d3f4bf6e0dff44acf64183fbd54d850921799029
Евгений Кривоногов <ekrivonogov@inbox.ru>
2023-12-28 05:36:36 +0200

    Щупаем dir и разные методы

--- Distance.py
+++ Distance.py
@@ -9,16 +9,11 @@
 
 current_distance = input("Введи текущий километраж: ")
 print("current_distance : " + current_distance)
-print(type(current_distance))
+
+print(dir(current_distance))
 
 
-print("Пощупаем другие типы");
-var1 = 1;
-print(type(var1))
-var2 = True;
-print(type(var2))
-var3 = "test";
-print(type(var3))
+
 
 
 
